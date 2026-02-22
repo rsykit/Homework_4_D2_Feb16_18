808007e4935f8e9e86a16c0727cc7098a2e0700d
Kristy Chan <kristyccc0311@icloud.com>
2026-02-21 16:27:25 -0800

    Node class

--- src/assignment1.java
+++ src/assignment1.java
@@ -1,3 +1,13 @@
 public class assignment1 {
-    
+    static class Node 
+    {
+        int key;
+        Node next;
+
+        Node (int key) 
+        {
+            this.key = key;
+            this.next = null;
+        }
+    }
 }
